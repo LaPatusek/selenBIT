288444e798df25f5eabeae0c5f23866216740d28
LaPatusek <ddrrusek@gmail.com>
2023-03-05 22:58:30 +0100

    testujemy

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Cennik from './Pages/cennik';
+import Kontakt from './Pages/kontakt';
+import Main from './Pages/Main';
+import Onas from './Pages/onas';
+import Szkolenia from './Pages/szkolenia';
+
+const App = () => {
+  return (
+    <Fragment>
+      <header>
+        <Header></Header>
+      </header>
+
+      <main>
+        <Switch>
+          <Route path='/main'>
+            <Main />
+          </Route>
+          <Route path='/szkolenia'>
+            <Szkolenia />
+          </Route>
+          <Route path='/cennik'>
+            <Cennik />
+          </Route>
+          <Route path='/o-nas'>
+            <Onas />
+          </Route>
+          <Route path='/kontakt'>
+            <Kontakt />
+          </Route>
+          <Route path='*'>
+            <Main />
+          </Route>
+        </Switch>
+      </main>
+
+      <Footer></Footer>
+    </Fragment>
+  );
+};
+
+export default App;
---
+++ src/Pages/cennik.jsx
@@ -0,0 +1,5 @@
+const Cennik = () => {
+  return <h1> cennik testowy</h1>;
+};
+
+export default Cennik;
