f5f1e6ccfbffc5a902e51bb0c613c29d0ffd4f34
LaPatusek <ddrrusek@gmail.com>
2023-03-07 17:31:40 +0100

    v0.46

---
+++ src/Components/Elements/animatedBackground.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+import styles from './animatedBackground.module.css';
+
+const AnimatedBackground = () => {
+  return (
+    <section className={styles['stars-background']}>
+      <div className={styles.stars} />
+      <div className={styles.stars2} />
+      <div className={styles.stars3} />
+      <div className={styles.text}>
+        <h3>Rozwiązania IT którzych potrzebujesz</h3>
+        <h1>Przyszłość zaczyna się tutaj</h1>
+        <Link to='/szkolenia' className={styles['main-button']}> nasza oferta</Link>
+      </div>
+    </section>
+  );
+};
+
+export default AnimatedBackground;
