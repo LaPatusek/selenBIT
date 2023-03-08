7cf617204f15f5fd9889d8965255078a8d5c66e2
LaPatusek <ddrrusek@gmail.com>
2023-03-08 22:07:15 +0100

    v0.479

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,7 +1,9 @@
 import { Fragment } from 'react';
-
+import thirdIcon from '../Assets/human-icon.png';
+import chwilowe from '../Assets/meeting-photo.jpg';
+import firstIcon from '../Assets/migrate-icon.png';
+import secondIcon from '../Assets/target-icon.png';
 import AnimatedBackground from '../Components/Elements/animatedBackground';
-
 import styles from './main.module.css';
 
 const Main = () => {
@@ -22,7 +24,8 @@ const Main = () => {
         </div>
 
         <div className={styles['second-section-boxes']}>
-          <div className={styles['first-box']}>
+          <div className={styles['content-box']}>
+            <img src={firstIcon} alt='' width='110px' />
             <h3>Jesteśmy mobilni</h3>
             <h2>Przyjedziemy do Ciebie</h2>
             <p>
@@ -30,7 +33,8 @@ const Main = () => {
               miejsce. Oferujemy także szkolenia zdalne.
             </p>
           </div>
-          <div className={styles['second-box']}>
+          <div className={styles['content-box']}>
+            <img src={secondIcon} alt='' width='110px' />
             <h3>Dopasujemy się do Ciebie</h3>
             <h2>Spełnimy Twoje oczekiwania</h2>
             <p>
@@ -38,7 +42,8 @@ const Main = () => {
               potrzeb.
             </p>
           </div>
-          <div className={styles['third-box']}>
+          <div className={styles['content-box']}>
+            <img src={thirdIcon} alt='' width='110px' />
             <h3>Inwidualne podejście</h3>
             <h2>Dopasujemy się do poziomu </h2>
             <p>
@@ -46,6 +51,27 @@ const Main = () => {
               uczestników. Bez obaw. Dostoujemy się.
             </p>
           </div>
+        </div>
+      </section>
+
+      <section className={styles['third-section']}>
+        <div className={styles['third-section--left-part']}>
+          <h2>O nas</h2>
+          <p>
+            Jesteśmy firmą działającą na rynku szkoleń od 2018 roku. Mamy bogate
+            doświadczenie w nauczaniu. Posiadamy wpis do Rejestru Instytucji
+            Szkoleniowych. <br /> Naszym sukcesem są zadowoleni kursanci.
+          </p>
+        </div>
+
+        <div className={styles['third-section--right-part']}>
+          <p>
+            Życie jest krótkie. Korzystaj z teraźniejszości w sposób rozumny i
+            słuszny. Bądź rozsądny w odpoczynku
+          </p>
+          {/* <hr /> */}
+          <p>Marcus Aurelius</p>
+          <img src={chwilowe} alt='' width='300px'/>
         </div>
       </section>
     </Fragment>
