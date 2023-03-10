1acc6d6d86927b2510b76cebe01e00a73a80a470
LaPatusek <ddrrusek@gmail.com>
2023-03-10 22:09:46 +0100

    v0.7

--- src/Pages/szkolenia.jsx
+++ src/Pages/szkolenia.jsx
@@ -1,13 +1,15 @@
-import { Fragment } from 'react';
+import styles from './szkolenia.module.css';
 
 const Szkolenia = () => {
   return (
-    <Fragment>
-      <div />
-      <h3> Szkolenia </h3>
-      <div />
-      <h1>Szkolenia informatyczne</h1>
-    </Fragment>
+    <div>
+      <div className={styles['page-title']}>
+        <div className={styles['display-flex']}>
+        <h3> Szkolenia </h3>
+        </div>
+        <h1>Szkolenia informatyczne</h1>
+      </div>
+    </div>
   );
 };
 
