51c403fa38185e76ebe81ca8d4aabbc5989c5093
LaPatusek <ddrrusek@gmail.com>
2023-03-14 13:40:27 +0100

    v0.88

--- src/Components/Elements/quote.js
+++ src/Components/Elements/quote.js
@@ -6,7 +6,7 @@ const Quote = () => {
       <div className={styles['blockquote']}>
         <h1>
           Życie jest krótkie. Korzystaj z teraźniejszości w sposób rozumny
-          <span>i słuszny. Bądź rozsądny w odpoczynku</span>
+          <span> i słuszny. Bądź rozsądny w odpoczynku</span>
         </h1>
         <h4>&mdash; Marcus Aurelius</h4>
       </div>
