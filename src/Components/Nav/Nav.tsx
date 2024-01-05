d9cb13cbed9c81f90fa84a2214da94a3672a800e
LaPatusek <ddrrusek@gmail.com>
2024-01-05 12:22:07 +0100

    navbar

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,5 +1,5 @@
 import { HambergerMenu } from 'iconsax-react';
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '../../Assets/cropped-logo11-300x147.webp';
 import styles from './Nav.module.css';
@@ -49,66 +49,81 @@ const Nav: React.FC = () => {
   }, [pathname]);
 
   return (
-    <Fragment>
-      <div
-        className={`${styles.header} ${
-          mainPageState ? styles['main-navbar'] : ''
-        }`}
-      >
-        <Link to='/'>
-          <img src={Logo} alt='logo' width='auto' height={'127px'} />
-        </Link>
+    <div
+      className={`${styles.header} ${
+        mainPageState ? styles['main-navbar'] : ''
+      }`}
+    >
+      <Link to='/'>
+        <img src={Logo} alt='logo' width='auto' height={'127px'} />
+      </Link>
 
-        <div className={`${styles.nav}`}>
-          <div className={styles['nav-links']}>
-            <Link to='/'>Strona główna</Link>
-            <Link to='/szkolenia'>Szkolenia</Link>
-            <Link to='/cennik'>Cennik</Link>
-            <Link to='/o-nas'>O nas</Link>
-            <Link to='/kontakt'>Kontakt</Link>
-          </div>
+      <div className={`${styles.nav}`}>
+        <div className={styles['nav-links']}>
+          <Link to='/'>Strona główna</Link>
+          <Link to='/szkolenia'>Szkolenia</Link>
+          <Link to='/cennik'>Cennik</Link>
+          <Link to='/o-nas'>O nas</Link>
+          <Link to='/kontakt'>Kontakt</Link>
         </div>
       </div>
+
       <div
         className={`${styles['navbar-800']} ${
           menuIsVis ? styles['active-menu-800'] : ''
         }`}
       >
-        <img src={Logo} alt='logo' height={'147px'} width={'300px'} />
-
         <div className={styles['hamburger-menu-icon']} onClick={menuHandler}>
-          <HambergerMenu size='32' />
+          {!menuIsVis && <HambergerMenu size='32' />}
+          {menuIsVis && (
+            <svg
+              fill='#ffffff'
+              height='24px'
+              width='24px'
+              viewBox='0 0 492 492'
+            >
+              <path
+                d='M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872
+			c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872
+			c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052
+			L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116
+			c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952
+			c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116
+			c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z'
+              />
+            </svg>
+          )}
         </div>
 
         {menuIsVis && (
-          <div className={`${styles['nav-menu-800']}`}>
+          <div className={`${styles['nav-menu-800']}`} onClick={menuHandler}>
             <ul>
               <li>
-                <Link to='/main' onClick={menuHandler} ref={linkRefOne}>
-                  Strona główna
+                <Link to='/main' ref={linkRefOne}>
+                  Strona <br /> główna
                 </Link>
               </li>
 
               <li>
-                <Link to='/szkolenia' onClick={menuHandler} ref={linkRefTwo}>
+                <Link to='/szkolenia' ref={linkRefTwo}>
                   szkolenia
                 </Link>
               </li>
 
               <li>
-                <Link to='/cennik' onClick={menuHandler} ref={linkRefThree}>
+                <Link to='/cennik' ref={linkRefThree}>
                   Cennik
                 </Link>
               </li>
 
               <li>
-                <Link to='/o-nas' onClick={menuHandler} ref={linkRefFour}>
+                <Link to='/o-nas' ref={linkRefFour}>
                   O nas
                 </Link>
               </li>
 
               <li>
-                <Link to='/kontakt' onClick={menuHandler} ref={linkRefFive}>
+                <Link to='/kontakt' ref={linkRefFive}>
                   kontakt
                 </Link>
               </li>
@@ -116,7 +131,7 @@ const Nav: React.FC = () => {
           </div>
         )}
       </div>
-    </Fragment>
+    </div>
   );
 };
 
